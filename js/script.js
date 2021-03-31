fea3bd1c26f0b80cf1628176bf1f0f37c240f32c
Waine Joy <wainejoy@yahoo.com.br>
2021-03-31 16:05:16 -0300

    definir movimentos da cobrinha

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criaBG(){
     context.fillStyle = "lightgreen";
@@ -19,6 +20,28 @@ function criarcobrinha(){
     }
 }
 
-criaBG();
-criarcobrinha();
+function iniciarJogo(){
+    criaBG();
+    criarcobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        Y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
 
